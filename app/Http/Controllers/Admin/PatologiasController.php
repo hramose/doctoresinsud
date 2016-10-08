8f04286bffdf36147970e4dc710dda124b996601
Alejandro Abraham <alejandro_abraham@me.com>
2016-10-08 22:05:38 +0000

    complementa patologias

---
+++ app/Http/Controllers/Admin/PatologiasController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Patologia;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PatologiasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        //Muestra patologias del sistema
+        $patologias = Patologia::all();
+        return view('backend.patologias.index', compact('patologias'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //Devuelve vista para crear una nueva patología
+        return view('backend.patologias.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Guarda datos del formulario de patologias
+        $patologia = new Patologia(array(
+            'nombre' => $request->get('nombre'),
+            'descripcion' => $request->get('descripcion'),
+        ));
+
+        $patologia->save();
+
+        return redirect('/admin/patologias/')->with('status', 'La nueva patología se creó correctamente.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
